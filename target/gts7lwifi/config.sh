c3309741dee920272e3fba30e8f95f11b620c5fb
ssch71 <s01093450498@gmail.com>
2026-06-04 00:30:12 +0900

    gts7lwifi/config.sh 만들기

---
+++ target/gts7lwifi/config.sh
@@ -0,0 +1,11 @@
+# Copyright (c) 2025 Salvo Giangreco
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# Device configuration file for Galaxy Tab S7 (WIFI) (gts7wifi)
+TARGET_NAME="Galaxy Tab S7 (WIFI)"
+TARGET_CODENAME="gts7lwifi"
+TARGET_ASSERT_MODEL=("SM-T870")
+TARGET_PLATFORM="SM8250"
+TARGET_FIRMWARE="SM-T870/XAC/R52N91CH6ZY"
+TARGET_EXTRA_FIRMWARES=()
+TARGET_IS_TABLET=true
