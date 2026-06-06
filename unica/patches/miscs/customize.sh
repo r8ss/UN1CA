706997f8271e4ee019af52014db065726227b643
ssch71 <s01093450498@gmail.com>
2026-06-06 18:04:19 +0900

    customize.sh 업데이트

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -1,3 +1,5 @@
+SKIPUNZIP=1
+
 SET_PROP_IF_DIFF "vendor" "ro.oem_unlock_supported" "0"
 
 # Better device/model detection in CoreRune
@@ -24,3 +26,41 @@ SMALI_PATCH "system" "system/framework/services.jar" \
     '<clinit>()V' \
     'ro.product.model' \
     'ro.product.vendor.model'
+
+# Disable vendor mismatch warning (API 36+)
+if [ "$SOURCE_PLATFORM_SDK_VERSION" -ge "36" ]; then
+    APPLY_PATCH "system" "system/framework/services.jar" \
+        "$MODPATH/smali/system/framework/services.jar/0001-Disable-vendor-mismatch-warning.patch"
+fi
+SET_PROP_IF_DIFF "vendor" "ro.oem_unlock_supported" "0"
+
+# Better device/model detection in CoreRune
+SMALI_PATCH "system" "system/framework/framework.jar" \
+    "smali_classes6/com/samsung/android/rune/CoreRune.smali" "replace" \
+    '<clinit>()V' \
+    'ro.product.model' \
+    'ro.product.vendor.model'
+SMALI_PATCH "system" "system/framework/framework.jar" \
+    "smali_classes6/com/samsung/android/rune/CoreRune.smali" "replace" \
+    '<clinit>()V' \
+    'ro.product.device' \
+    'ro.product.vendor.device'
+
+# Disable RescueParty
+SMALI_PATCH "system" "system/framework/services.jar" \
+    "smali/com/android/server/RescueParty.smali" "return" \
+    '-$$Nest$smisDisabled()Z' \
+    'true'
+
+# Better model detection in FreecessController
+SMALI_PATCH "system" "system/framework/services.jar" \
+    "smali/com/android/server/am/FreecessController.smali" "replace" \
+    '<clinit>()V' \
+    'ro.product.model' \
+    'ro.product.vendor.model'
+
+# Disable vendor mismatch warning (API 36+)
+if [ "$SOURCE_PLATFORM_SDK_VERSION" -ge "36" ]; then
+    APPLY_PATCH "system" "system/framework/services.jar" \
+        "$MODPATH/smali/system/framework/services.jar/0001-Disable-vendor-mismatch-warning.patch"
+fi
