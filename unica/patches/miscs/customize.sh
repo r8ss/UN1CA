728d5b43ad70ef2e13c354807e8eb3566b642ef0
ssch71 <s01093450498@gmail.com>
2026-06-06 23:48:26 +0900

    customize.sh 업데이트

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -27,31 +27,6 @@ SMALI_PATCH "system" "system/framework/services.jar" \
     'ro.product.model' \
     'ro.product.vendor.model'
 
-# Better device/model detection in CoreRune
-SMALI_PATCH "system" "system/framework/framework.jar" \
-    "smali_classes6/com/samsung/android/rune/CoreRune.smali" "replace" \
-    '<clinit>()V' \
-    'ro.product.model' \
-    'ro.product.vendor.model'
-SMALI_PATCH "system" "system/framework/framework.jar" \
-    "smali_classes6/com/samsung/android/rune/CoreRune.smali" "replace" \
-    '<clinit>()V' \
-    'ro.product.device' \
-    'ro.product.vendor.device'
-
-# Disable RescueParty
-SMALI_PATCH "system" "system/framework/services.jar" \
-    "smali/com/android/server/RescueParty.smali" "return" \
-    '-$$Nest$smisDisabled()Z' \
-    'true'
-
-# Better model detection in FreecessController
-SMALI_PATCH "system" "system/framework/services.jar" \
-    "smali/com/android/server/am/FreecessController.smali" "replace" \
-    '<clinit>()V' \
-    'ro.product.model' \
-    'ro.product.vendor.model'
-
 # Disable vendor mismatch warning
 SMALI_PATCH "system" "system/framework/services.jar" \
     "smali/com/android/server/am/ActivityManagerService\$\$ExternalSyntheticLambda6.smali" "replaceall" \
