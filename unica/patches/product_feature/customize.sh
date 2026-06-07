90539e4fbbb0304c791c26e3cc55f39d40a22b65
ssch71 <s01093450498@gmail.com>
2026-06-07 17:21:05 +0900

    customize.sh 업데이트

--- unica/patches/product_feature/customize.sh
+++ unica/patches/product_feature/customize.sh
@@ -71,16 +71,16 @@ LOG_MISSING_PATCHES()
 # SEC_PRODUCT_FEATURE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION
 if [[ "$SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" != "$TARGET_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" ]]; then
     if [[ "$SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" != "none" ]]; then
-        SMALI_PATCH "system" "system/framework/framework.jar" \
-            "smali_classes6/com/samsung/android/camera/mic/SemMultiMicManager.smali" "replace" \
-            "isSupported()Z" \
-            "$SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" \
-            "${TARGET_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION//none/}"
-        SMALI_PATCH "system" "system/framework/framework.jar" \
-            "smali_classes6/com/samsung/android/camera/mic/SemMultiMicManager.smali" "replace" \
-            "isSupported(I)Z" \
-            "$SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" \
-            "${TARGET_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION//none/}"
+        # SMALI_PATCH "system" "system/framework/framework.jar" \
+        #     "smali_classes6/com/samsung/android/camera/mic/SemMultiMicManager.smali" "replace" \
+        #     "isSupported()Z" \
+        #     "$SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" \
+        #     "${TARGET_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION//none/}"
+        # SMALI_PATCH "system" "system/framework/framework.jar" \
+        #     "smali_classes6/com/samsung/android/camera/mic/SemMultiMicManager.smali" "replace" \
+        #     "isSupported(I)Z" \
+        #     "$SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" \
+        #     "${TARGET_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION//none/}"
     else
         # TODO handle this condition
         LOG_MISSING_PATCHES "SOURCE_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION" "TARGET_AUDIO_CONFIG_RECORDALIVE_LIB_VERSION"
