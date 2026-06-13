7b513ddc546bd24cfcf0f85369da181257e445b4
ssch71 <s01093450498@gmail.com>
2026-06-14 01:52:10 +0900

    debloat.sh 업데이트

--- target/gts7lwifi/debloat.sh
+++ target/gts7lwifi/debloat.sh
@@ -3,64 +3,76 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-# 1. System
+# 1. System 파티션 (맨 앞 system/ 유지 및 고용량 추가)
 SYSTEM_DEBLOAT+="
-app/BixbyWakeUp
-app/KidsHome_Alpha
-app/SamsungTrends3
-app/StickerCenter
-app/AvatarEmojiSticker
-app/SamsungMax
-app/GalaxyWatchRROverlay
-app/WifiRROverlayAppLls
-priv-app/Bixby
-priv-app/BixbyService
-priv-app/SamsungSocial
-priv-app/SecureFolder
-priv-app/SamsungPass
-priv-app/SamsungPassAutofill
-priv-app/LedCoverService
-priv-app/DigitalWellbeing
-priv-app/DexDevelopment
-bin/mafpc_write
-bin/dhkprov
-bin/qchdcpkprov
-etc/init/dhkprov.rc
-lib64/vendor.samsung.hardware.security.hdcp.keyprovisioning@1.0.so
+system/app/BixbyWakeUp
+system/app/KidsHome_Alpha
+system/app/SamsungTrends3
+system/app/StickerCenter
+system/app/AvatarEmojiSticker
+system/app/SamsungMax
+system/app/GalaxyWatchRROverlay
+system/app/WifiRROverlayAppLls
+system/app/Scribe
+system/app/BluetoothMidiService
+system/priv-app/Bixby
+system/priv-app/BixbyService
+system/priv-app/SamsungSocial
+system/priv-app/SecureFolder
+system/priv-app/SamsungPass
+system/priv-app/SamsungPassAutofill
+system/priv-app/LedCoverService
+system/priv-app/DigitalWellbeing
+system/priv-app/DexDevelopment
+system/priv-app/LinkToWindowsService
+system/priv-app/YourPhoneCompanionSoftware
+system/priv-app/SmartSwitchAssistant
+system/priv-app/SamsungCompass
+system/bin/mafpc_write
+system/bin/dhkprov
+system/bin/qchdcpkprov
+system/etc/init/dhkprov.rc
+system/lib64/vendor.samsung.hardware.security.hdcp.keyprovisioning@1.0.so
 "
 
-# 2. Product
+# 2. Product 파티션 (맨 앞 product/ 붙이기)
 PRODUCT_DEBLOAT+="
-app/Maps
-app/YouTube
-app/Gmail2
-app/CalendarGoogle
-app/Chrome
-app/Photos
-app/Videos
-app/SamsungMusic
-app/SamsungNotes
-app/SamsungFree
-app/SamsungGlobalGoals
-app/SamsungShop
-app/SamsungMembers
-app/PreloadInstaller
-priv-app/Velvet
-priv-app/GmsCoreCloudSync
-priv-app/Turbo
-priv-app/Tips
-priv-app/MainlineScf
+product/app/Maps
+product/app/YouTube
+product/app/Gmail2
+product/app/CalendarGoogle
+product/app/Chrome
+product/app/Photos
+product/app/Videos
+product/app/SamsungMusic
+product/app/SamsungNotes
+product/app/SamsungFree
+product/app/SamsungGlobalGoals
+product/app/SamsungShop
+product/app/SamsungMembers
+product/app/PreloadInstaller
+product/app/AREmoji
+product/app/AvatarEmojiEditor
+product/app/EmojiUpdater
+product/app/WallpaperEffects
+product/priv-app/Velvet
+product/priv-app/GmsCoreCloudSync
+product/priv-app/Turbo
+product/priv-app/Tips
+product/priv-app/MainlineScf
+product/priv-app/SamsungCloud
+product/priv-app/Upday
 "
 
-# 3. System_ext 영역 삭제
+# 3. System_ext 영역 (맨 앞 system_ext/ 붙이기)
 SYSTEM_EXT_DEBLOAT+="
-etc/permissions/com.qti.location.sdk.xml
-etc/permissions/com.qualcomm.location.xml
-etc/permissions/privapp-permissions-com.qualcomm.location.xml
-framework/com.qti.location.sdk.jar
-priv-app/com.qualcomm.location
+system_ext/etc/permissions/com.qti.location.sdk.xml
+system_ext/etc/permissions/com.qualcomm.location.xml
+system_ext/etc/permissions/privapp-permissions-com.qualcomm.location.xml
+system_ext/framework/com.qti.location.sdk.jar
+system_ext/priv-app/com.qualcomm.location
+system_ext/priv-app/Pluspot
 "
-
 
 # 4. Overlays
 SYSTEM_DEBLOAT+="
