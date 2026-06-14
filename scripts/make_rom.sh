253e07f0cbf1dd6a7df96bb56ec327d73e4c5ff2
ssch71 <s01093450498@gmail.com>
2026-06-14 21:47:37 +0900

    Disable building flashable ZIP in make_rom.sh

--- scripts/make_rom.sh
+++ scripts/make_rom.sh
@@ -7,7 +7,7 @@ source "$SRC_DIR/scripts/utils/build_utils.sh" || exit 1
 FORCE=false
 BUILD_ROM=false
 BUILD_TARGET_FILES=true
-BUILD_FLASHABLE_ZIP=true
+BUILD_FLASHABLE_ZIP=false
 
 START_TIME="$(date +%s)"
 
