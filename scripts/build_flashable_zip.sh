032443cb6712ff0e5c0f362abc4c5057a61e0486
ssch71 <s01093450498@gmail.com>
2026-06-16 08:28:52 +0900

    build_flashable_zip.sh 업데이트

--- scripts/build_flashable_zip.sh
+++ scripts/build_flashable_zip.sh
@@ -1,181 +1,98 @@
 #!/usr/bin/env bash
-# Copyright (c) 2025 Salvo Giangreco
+# Copyright (c) 2026 Salvo Giangreco
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # [
 source "$SRC_DIR/scripts/utils/build_utils.sh" || exit 1
 
-FORCE=false
-BUILD_ROM=false
-BUILD_TARGET_FILES=true
-BUILD_FLASHABLE_ZIP=false
-
-START_TIME="$(date +%s)"
-
-SOURCE_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$SOURCE_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$SOURCE_FIRMWARE")"
-TARGET_FIRMWARE_PATH="$(cut -d "/" -f 1 -s <<< "$TARGET_FIRMWARE")_$(cut -d "/" -f 2 -s <<< "$TARGET_FIRMWARE")"
-
-GET_WORK_DIR_HASH()
-{
-    find "$SRC_DIR/unica" "$SRC_DIR/target/$TARGET_CODENAME" -type f -print0 | \
-        sort -z | xargs -0 sha1sum | sha1sum | cut -d " " -f 1
-}
+INCREMENTAL=false
+SOURCE_ZIP=""
+TARGET_ZIP=""
+OUTPUT_FILE=""
 
 PREPARE_SCRIPT()
 {
-    while [ "$#" != 0 ]; do
-        if [[ "$1" == "--force" ]] || [[ "$1" == "-f" ]]; then
-            FORCE=true
-        elif [[ "$1" == "--no-target-files" ]] || [[ "$1" == "-x" ]]; then
-            BUILD_TARGET_FILES=false
-            BUILD_FLASHABLE_ZIP=false
-        elif [[ "$1" == "--build-rom-zip" ]] || [[ "$1" == "-z" ]]; then
-            BUILD_TARGET_FILES=true
-            BUILD_FLASHABLE_ZIP=true
-        else
-            if [[ "$1" == "-"* ]]; then
-                LOGE "Unknown option: $1"
+    if [[ "$#" == 0 ]]; then
+        PRINT_USAGE
+        exit 1
+    fi
+
+    while [[ "$1" == "-"* ]]; do
+        if [[ "$1" == "--incremental" ]] || [[ "$1" == "-i" ]]; then
+            INCREMENTAL=true
+            shift; SOURCE_ZIP="$1"
+        elif [[ "$1" == "--output" ]] || [[ "$1" == "-o" ]]; then
+            shift; OUTPUT_FILE="$1"
+            if [[ "$OUTPUT_FILE" != *".zip" ]]; then
+                LOGE "Output file name must have \".zip\" extension"
+                exit 1
             fi
-            PRINT_USAGE
+        else
+            LOGE "Unknown option: $1"
             exit 1
         fi
 
         shift
     done
-}
 
-PRINT_BUILD_OUTCOME()
-{
-    local EXIT_CODE="$?"
-    local END_TIME
-    local ESTIMATED
-
-    END_TIME="$(date +%s)"
-    ESTIMATED="$((END_TIME - START_TIME))"
-
-    if [ "$EXIT_CODE" != "0" ]; then
-        echo -n -e '\n\033[1;31m'"Build failed "
-    else
-        echo -n -e '\n\033[1;32m'"Build completed "
+    TARGET_ZIP="$1"
+    if [ ! "$TARGET_ZIP" ]; then
+        PRINT_USAGE
+        exit 1
+    elif [ ! -f "$TARGET_ZIP" ]; then
+        LOGE "File not found: ${TARGET_ZIP//$SRC_DIR\//}"
+        exit 1
     fi
-    echo -e "in $((ESTIMATED / 3600))hrs $(((ESTIMATED / 60) % 60))min $((ESTIMATED % 60))sec."'\033[0m\n'
+
+    if [ "$SOURCE_ZIP" ]; then
+        if [ ! -f "$SOURCE_ZIP" ]; then
+            LOGE "File not found: ${SOURCE_ZIP//$SRC_DIR\//}"
+            exit 1
+        fi
+    fi
+
+    if [ ! "$OUTPUT_FILE" ]; then
+        local TARGET_BUILD_INFO
+
+        EVAL "unzip -p \"$TARGET_ZIP\" \"build_info.txt\"" || exit 1
+        TARGET_BUILD_INFO="$(unzip -p "$TARGET_ZIP" "build_info.txt")"
+
+        OUTPUT_FILE="$OUT_DIR/UN1CA_"
+        OUTPUT_FILE+="$(grep "^version" <<< "$TARGET_BUILD_INFO" | cut -d "=" -f 2 -s)"
+        OUTPUT_FILE+="_"
+        OUTPUT_FILE+="$(date -d "@$(grep "^timestamp" <<< "$TARGET_BUILD_INFO" | cut -d "=" -f 2 -s)" "+%Y%m%d")"
+        OUTPUT_FILE+="_"
+        OUTPUT_FILE+="$(grep "^device" <<< "$TARGET_BUILD_INFO" | cut -d "=" -f 2 -s)"
+        if $INCREMENTAL; then
+            local SOURCE_BUILD_INFO
+
+            EVAL "unzip -p \"$SOURCE_ZIP\" \"build_info.txt\"" || exit 1
+            SOURCE_BUILD_INFO="$(unzip -p "$SOURCE_ZIP" "build_info.txt")"
+
+            OUTPUT_FILE+="-INCREMENTAL_"
+            OUTPUT_FILE+="$(grep "^timestamp" <<< "$SOURCE_BUILD_INFO" | cut -d "=" -f 2 -s)"
+        fi
+        if ! $DEBUG || $ROM_IS_OFFICIAL; then
+            OUTPUT_FILE+="-sign"
+        fi
+        OUTPUT_FILE+=".zip"
+    fi
 }
 
 PRINT_USAGE()
 {
-    echo "Usage: make_rom [options]" >&2
-    echo " -f, --force : Force ROM build" >&2
-    echo " -x, --no-target-files : Do not build target-files zip" >&2
-    echo " -z, --build-rom-zip : Build flashable zip" >&2
+    echo "Usage: build_flashable_zip [options] <file>" >&2
+    echo " -i, --incremental : Generate an incremental zip using the given target-files zip as source" >&2
+    echo " -o, --output : Specify the output zip path, defaults to $OUT_DIR" >&2
 }
 # ]
 
 PREPARE_SCRIPT "$@"
 
-if $FORCE; then
-    BUILD_ROM=true
+if $INCREMENTAL; then
+    "$SRC_DIR/scripts/internal/build_incremental_ota_zip.sh" "$SOURCE_ZIP" "$TARGET_ZIP" "$OUTPUT_FILE" || exit 1
 else
-    if [ -f "$WORK_DIR/.completed" ]; then
-        if [[ "$(cat "$WORK_DIR/.completed")" == "$(GET_WORK_DIR_HASH)" ]]; then
-            LOGW "No changes have been detected in the build environment"
-            BUILD_ROM=false
-        else
-            LOGW "Changes detected in the build environment"
-            BUILD_ROM=true
-        fi
-    else
-        BUILD_ROM=true
-    fi
-fi
-
-trap 'PRINT_BUILD_OUTCOME' EXIT
-trap 'echo' INT
-
-if $BUILD_ROM; then
-    [ -d "$APKTOOL_DIR" ] && rm -rf "$APKTOOL_DIR"
-    [ -f "$WORK_DIR/.completed" ] && rm -f "$WORK_DIR/.completed"
-
-    if [ ! -f "$FW_DIR/$SOURCE_FIRMWARE_PATH/.extracted" ] || [ ! -f "$FW_DIR/$TARGET_FIRMWARE_PATH/.extracted" ]; then
-        if [ ! -f "$ODIN_DIR/$SOURCE_FIRMWARE_PATH/.downloaded" ] || [ ! -f "$ODIN_DIR/$TARGET_FIRMWARE_PATH/.downloaded" ]; then
-            LOG_STEP_IN true "Downloading required firmwares"
-            "$SRC_DIR/scripts/download_fw.sh" || exit 1
-            LOG_STEP_OUT
-        fi
-        LOG_STEP_IN true "Extracting required firmwares"
-        "$SRC_DIR/scripts/extract_fw.sh" || exit 1
-        LOG_STEP_OUT
-    fi
-
-    LOG_STEP_IN true "Creating work dir"
-    "$SRC_DIR/scripts/internal/create_work_dir.sh" || exit 1
-    LOG_STEP_OUT
-
-    if [ -d "$SRC_DIR/platform/$TARGET_PLATFORM/patches" ]; then
-        LOG_STEP_IN true "Applying platform patches"
-        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/platform/$TARGET_PLATFORM/patches" || exit 1
-        LOG_STEP_OUT
-    fi
-    if [ -d "$SRC_DIR/target/$TARGET_CODENAME/patches" ]; then
-        LOG_STEP_IN true "Applying device patches"
-        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/target/$TARGET_CODENAME/patches" || exit 1
-        LOG_STEP_OUT
-    fi
-    if [ -d "$SRC_DIR/unica/patches" ]; then
-        LOG_STEP_IN true "Applying ROM patches"
-        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/unica/patches" || exit 1
-        LOG_STEP_OUT
-    fi
-
-    if [ -d "$SRC_DIR/unica/mods" ]; then
-        LOG_STEP_IN true "Applying ROM mods"
-        "$SRC_DIR/scripts/internal/apply_modules.sh" "$SRC_DIR/unica/mods" || exit 1
-        LOG_STEP_OUT
-    fi
-
-    if [ -d "$APKTOOL_DIR" ]; then
-        LOG_STEP_IN true "Building APKs/JARs"
-
-        while IFS= read -r f; do
-            f="${f/$APKTOOL_DIR\//}"
-            PARTITION="$(cut -d "/" -f 1 -s <<< "$f")"
-            if [[ "$PARTITION" == "system" ]]; then
-                "$SRC_DIR/scripts/apktool.sh" b "system" "$f" &
-            else
-                "$SRC_DIR/scripts/apktool.sh" b "$PARTITION" "$(cut -d "/" -f 2- -s <<< "$f")" &
-            fi
-        done < <(find "$APKTOOL_DIR" -type d \( -name "*.apk" -o -name "*.jar" \))
-
-        # shellcheck disable=SC2046
-        wait $(jobs -p) || exit 1
-
-        LOG_STEP_OUT
-    fi
-
-    echo -n "$(GET_WORK_DIR_HASH)" > "$WORK_DIR/.completed"
-fi
-
-if $BUILD_TARGET_FILES || $BUILD_FLASHABLE_ZIP; then
-    ZIP_FILE_NAME="${TARGET_CODENAME}_"
-    if [ "$(GET_PROP "system" "ro.unica.version")" ]; then
-        ZIP_FILE_NAME+="$(GET_PROP "system" "ro.unica.version")"
-    else
-        ZIP_FILE_NAME+="$ROM_VERSION"
-    fi
-    ZIP_FILE_NAME+="-target_files.zip"
-
-    if [ ! -f "$OUT_DIR/$ZIP_FILE_NAME" ]; then
-        LOG_STEP_IN true "Creating target-files zip"
-        "$SRC_DIR/scripts/internal/create_target_files_zip.sh" "$OUT_DIR/$ZIP_FILE_NAME" || exit 1
-        LOG_STEP_OUT
-    else
-        LOGW "File already exists: ${OUT_DIR//$SRC_DIR\//}/$ZIP_FILE_NAME"
-    fi
-
-    if $BUILD_FLASHABLE_ZIP; then
-        LOG_STEP_IN true "Creating flashable zip"
-        "$SRC_DIR/scripts/build_flashable_zip.sh" "$OUT_DIR/$ZIP_FILE_NAME" || exit 1
-        LOG_STEP_OUT
-    fi
+    "$SRC_DIR/scripts/internal/build_full_ota_zip.sh" "$TARGET_ZIP" "$OUTPUT_FILE" || exit 1
 fi
 
 exit 0
