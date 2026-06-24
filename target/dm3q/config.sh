3d5a2b1babcca00b49f29ff8b0e5a43f96c101f6
ssch71 <s01093450498@gmail.com>
2026-06-24 15:32:18 +0900

    dm3q/config.sh 만들기

---
+++ target/dm3q/config.sh
@@ -0,0 +1,21 @@
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S23 Ultra (dm3q)
+TARGET_NAME="Galaxy S23 Ultra"
+TARGET_CODENAME="dm3q"
+TARGET_PLATFORM="sm8550"
+TARGET_FIRMWARE="SM-S918B/EUX/350196551234562"
+TARGET_EXTRA_FIRMWARES=()
+
+# SEC Product Feature
+TARGET_COMMON_SUPPORT_DYN_RESOLUTION_CONTROL=true
+TARGET_DVFSAPP_CONFIG_SSRM_POLICY_FILENAME="siop_dm3q_sm8550"
+TARGET_LCD_CONFIG_HFR_MODE="3"
+TARGET_WLAN_SUPPORT_MOBILEAP_DUALAP=false
